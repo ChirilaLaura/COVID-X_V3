6e20cec2e026f29ee5e3949d8213ee397bcd9892
Cristea Luca <lucacristea04@gmail.com>
2020-04-19 00:28:55 +0300

    changes at analizeaza_rmn(without beeing connected to flask)

--- app.py
+++ app.py
@@ -1,25 +1,26 @@
-from flask import Flask, render_template, request
-#from keras.models import load_model
-#from keras.backend import set_session
-#from skimage.transfrom import resize
-#import tensorflow as tf
+from flask import Flask, render_template, request, redirect, url_for,  Response, jsonify
+from werkzeug.utils import secure_filename
+from gevent.pywsgi import WSGIServer
+
+import tensorflow as tf
+from tensorflow import keras
+from keras.applications.vgg16 import preprocess_input, decode_predictions
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+
+import os, sys
 import numpy as np
-import os, requests
-
-#global sess
-#sess = tf.Session()
-#set_session(sess)
-#model = tf.keras.models.load_model('model.h5', custom_objects={'Adam': lambda **kwargs: hvd.DistributedOptimizer(keras.optimizers.Adam(**kwargs))})
-#global graph
-#graph = tf.get_default_graph()
-
+from util import base64_to_pil
 
 
 app = Flask(__name__)
+MODEL = 'TestAI/model.h5'
+model = load_model(MODEL)
+model._make_predict_function()
+print("loaded")
 
 @app.route('/')
 def index():
-    #css(flips cards, style), home.html
     return render_template('home.html')
 
 @app.route('/CovidX/Analizeaza_RMN')
@@ -37,17 +38,7 @@ def info():
 @app.route('/CovidX/numar_cazuri')
 def infected():
     return render_template('numar_cazuri.html')
-'''
-@app.route('/mri_scan', methods=['POST'])
-def request_fisier():
-    form = request.form
-    if request.method == 'POST':
-        file = request.files['file']
-        filename = secure_filename(file.filename)
-        file.save(os.path.join('uploads', filename))
-        return redirect(url_for('prediction', filename=filename))
-    return render_template("<<fisier_request.html>>")
-'''
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/js/mri_scan_image.js
+++ static/js/mri_scan_image.js
@@ -1,27 +1,120 @@
-//.text-success {
-//  color: #28a745 !important; }
+var fileDrag  = document.getElementById("file-drag");
+var fileSelect = document.getElementById("file-upload");
 
-//.text-danger {
-//color: #dc3545 !important; }
-$(document).ready(function () {
-  var onCompletion = (res) => {
-    consolog.log('Gata1')
-    res = JSON.parse(res);
-    prediction = res.prediction;
-    console.log("Predictie" + prediction + "tip_date=" + (typeof prediction) + "rezultat" + (prediction==1))
-    res1 = res.image
-    $("#image-box").attr("src", "data:image/jpeg;charset=utf-8;base64," + res1);
-    if(prediction == 1) {
-      let rezultat_text = '<p>In urma scanarii plamaniilor, rezulta ca pacientul este infectat</p>';
-      $('#result').removeClass('text-success').addClass('text-danger').html(rezultat_text);
-    } else {
-      let rezultat_text = '</p> In urma scanarii plamaniilor, rezulta ca pacientul este sanatos</p>';
-      $('#result').removeClass('text-danger').addClass('text-success').html(rezultat_text);
-    }
-    $('#result-heading').show();
-    $('#result-row').show();
+fileDrag.addEventListener("dragover", fileDragHover, false);
+fileDrag.addEventListener("dragleave", fileDragHover, false);
+fileDrag.addEventListener("drop", fileSelectHandler, false);
+fileDrag.addEventListener("change", fileSelectHandler, false);
+
+function fileDragHover(e) {
+  e.preventDefault();
+  e.stopPropagation();
+
+  fileDrag.className = e.type === "dragover" ? "upload-box dragover" : "upload-box";
+}
+
+function fileSelectHandler(e) {
+  var files = e.target.files || e.dataTransfer.files;
+  fileDragHover(e);
+  for(var i = 0, f; (f = files[i]); i++) {
+    previewFile(f);
   }
-  $('#result-heading').hide();
-  $('#result-row').hide();
-  $('#progress-bar').hide();
-});
+}
+
+var imagePreview = document.getElementById("image-preview");
+var imageDisplay = document.getElementById("image-display");
+var uploadCaption = document.getElementById("upload-caption");
+var predResult = document.getElementById("pred-result");
+var loader = document.getElementById("loader");
+
+function submitImage() {
+  console.log("submit tbh");
+
+  if(!imageDisplay.src || !imageDisplay.src.startsWith("data")) {
+    window.alert("Esti gay!");
+    return ;
+  }
+
+  loader.classList.remove("hidden");
+  imageDisplay.classList.add("loading")
+
+  predictImage(imageDisplay.src);
+}
+
+
+function clearImage() {
+  fileSelect.value = "";
+
+  imagePreview.src = "";
+  imageDisplay.src = "";
+
+  predResult.innerHTML = "";
+
+  hide(imagePreview);
+  hide(imageDisplay);
+  hide(loader);
+  hide(predResult);
+  show(uploadCaption);
+
+  imageDisplay.classList.remove("loading");
+}
+
+function previewFile(file) {
+  console.log(file.name);
+  var fileName = encodeURI(file.name);
+
+  var reader = new FileReader();
+  reader.readAsDataURL(file);
+  reader.onloadend = () => {
+    imagePreview.src = URL.createObjectURL(file);
+
+    show(imagePreview);
+    hide(uploadCaption);
+
+    predResult.innerHTML = "";
+    imageDisplay.classList.remove("loading");
+
+    displayImage(reader.result, "image-display");
+
+  };
+}
+
+function predictImage(image) {
+  fetch("/CovidX/Analizeaza_RMN", {
+    method : "POST",
+    headers: {
+      "Content-Type" : "application/json"
+    },
+    body: JSON.stringify(image)
+  })
+    .then(resp => {
+      if(resp.ok)
+        resp.json().then(data => {
+          displayResult(data);
+        });
+      })
+      .catch(err => {
+        console.log("An error occured", err.message);
+        window.alert("Eroare, uita-te in consola");
+      });
+}
+
+function displayImage(image, id) {
+  let display = document.getElementById(id);
+  display.src = image;
+  show(display);
+}
+
+function displayResult(data) {
+  hide(loader);
+  predResult.innerHTML = data.result;
+  show(predResult);
+}
+
+function hide(el) {
+  el.classList.add("hidden");
+}
+
+function show(el) {
+  el.classList.remove("hidden");
+}
